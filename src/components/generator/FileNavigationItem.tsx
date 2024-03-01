e658d8b65cae65cd616ac750be25446b0dd074b3
Marco Link <marco.link@contentful.com>
2024-03-01 14:10:37 +0100

    feat: response type renderer (#687)
    * feat: respnse type renderer
    
    * chore: update browserlist

--- src/components/generator/FileNavigationItem.tsx
+++ src/components/generator/FileNavigationItem.tsx
@@ -1,27 +1,32 @@
-import { TextLink } from "@contentful/f36-components";
+import {TextLink} from "@contentful/f36-components";
 import tokens from "@contentful/f36-tokens";
 import {css} from "emotion";
 import * as React from "react";
 
 const styles = {
-    link: css({
-        marginBottom: tokens.fontSizeS
-    })
+  link: css({
+    marginBottom: tokens.fontSizeS,
+    fontFamily: 'monospace',
+    fontSize: 'small'
+  })
 }
 
 type Props = {
-    file: string
-    onSelect: (file: string) => void;
-    selected: boolean;
+  file: string
+  onSelect: (file: string) => void;
+  selected: boolean;
 };
 
 const FileNavigationItem: React.FC<Props> = ({file, onSelect, selected}) => {
-    return (
-        <div>
-            <TextLink variant={selected ? "positive" : 'primary'} className={styles.link}
-                      onClick={() => onSelect(file)}>{file}</TextLink>
-        </div>
-    );
+  return (
+    <div>
+      <TextLink
+        variant={selected ? "positive" : 'primary'}
+        className={styles.link}
+        onClick={() => onSelect(file)}
+      >{file}</TextLink>
+    </div>
+  );
 };
 
 export default FileNavigationItem;
