223567def6bd25630f62a88b456102217bd038de
Marco Link <marco.link@contentful.com>
2024-08-19 19:50:15 +0200

    fix: require vs import

--- src/locations/Page.tsx
+++ src/locations/Page.tsx
@@ -1,4 +1,3 @@
-import {PageExtensionSDK} from '@contentful/app-sdk';
 import {
     Checkbox,
     CopyButton,
@@ -27,214 +26,217 @@ import SidebarSection from "../components/generator/SidebarSection";
 import {Flag, useBuilder} from "../components/generator/useBuilder";
 import {useMultiFileContent} from "../components/generator/useMulitFileContent";
 import {useSingleFileContent} from "../components/generator/useSingleFileContent";
+import {PageAppSDK} from "@contentful/app-sdk";
 
-require('prismjs/components/prism-typescript');
+// @ts-ignore
+import prismjs from "prismjs/components/prism-typescript";
 
 const styles = {
-    sidebarHeadline: css({
-        fontFamily: '-apple-system, system-ui, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-        letterSpacing: '1px',
-        fontSize: '0.75em',
-        borderBottom: '1px solid var(--color-element-base)',
-        lineHeight: '24px',
-        textTransform: 'uppercase',
-        color: 'rgb(106, 120, 137)',
-        borderBottomColor: 'rgb(195, 207, 213)',
-        borderBottomStyle: 'solid',
-        borderBottomWidth: '1px',
-        marginTop: '23px',
-        marginBlockStart: '0.83em',
-        marginBlockEnd: '0.83em',
-        marginBottom: '23px',
-        paddingTop: '24px',
-        boxSizing: 'border-box'
-    }),
+  sidebarHeadline: css({
+    fontFamily: '-apple-system, system-ui, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+    letterSpacing: '1px',
+    fontSize: '0.75em',
+    borderBottom: '1px solid var(--color-element-base)',
+    lineHeight: '24px',
+    textTransform: 'uppercase',
+    color: 'rgb(106, 120, 137)',
+    borderBottomColor: 'rgb(195, 207, 213)',
+    borderBottomStyle: 'solid',
+    borderBottomWidth: '1px',
+    marginTop: '23px',
+    marginBlockStart: '0.83em',
+    marginBlockEnd: '0.83em',
+    marginBottom: '23px',
+    paddingTop: '24px',
+    boxSizing: 'border-box'
+  }),
 
-    downloadButton: css({
-        marginBottom: '8px'
-    }),
-    copyButton: css({
-        float: 'right',
-        marginBottom: '-20px',
-        marginLeft: '-1px',
-        borderTopLeftRadius : '0',
-        borderBottomLeftRadius : '0'
-    })
+  downloadButton: css({
+    marginBottom: '8px'
+  }),
+  copyButton: css({
+    float: 'right',
+    marginBottom: '-20px',
+    marginLeft: '-1px',
+    borderTopLeftRadius: '0',
+    borderBottomLeftRadius: '0'
+  })
 }
 
 const SINGLE_FILE_NAME = 'content-types.ts';
 
 
 const Page: React.FC = () => {
-    const sdk = useSDK<PageExtensionSDK>();
-    const cma = useCMA();
-    const [output, setOutput] = useState('')
-    const [selectedFile, setSelectedFile] = useState<string | undefined>(SINGLE_FILE_NAME)
-    const [flags, setFlags] = useState<Flag[]>([]);
+  const sdk = useSDK<PageAppSDK>();
+  const cma = useCMA();
+  const [output, setOutput] = useState('')
+  const [selectedFile, setSelectedFile] = useState<string | undefined>(SINGLE_FILE_NAME)
+  const [flags, setFlags] = useState<Flag[]>([]);
 
-    const [isV10, setIsV10] = useState(true)
+  const [isV10, setIsV10] = useState(true)
 
-    const {data: contentTypesData} = useQuery({
-        queryKey: [sdk.ids.space, sdk.ids.environment, 'content-types'],
-        queryFn: () => cma.contentType.getMany({}),
-    })
+  const {data: contentTypesData} = useQuery({
+    queryKey: [sdk.ids.space, sdk.ids.environment, 'content-types'],
+    queryFn: () => cma.contentType.getMany({}),
+  })
 
-    const {
-        data: userData,
-        isLoading: isLoadingUsers,
-    } = useQuery({
-        queryKey: [sdk.ids.space, 'users'],
-        queryFn: () => cma.user.getManyForSpace({})
-    })
+  const {
+    data: userData,
+    isLoading: isLoadingUsers,
+  } = useQuery({
+    queryKey: [sdk.ids.space, 'users'],
+    queryFn: () => cma.user.getManyForSpace({})
+  })
 
-    const builder = useBuilder({
-        contentTypes: contentTypesData?.items || [],
-        users: userData?.items || [],
-        flags,
-        isV10
-    })
+  const builder = useBuilder({
+    contentTypes: contentTypesData?.items || [],
+    users: userData?.items || [],
+    flags,
+    isV10
+  })
 
-    const files = useMultiFileContent(builder)
-    const singleFileContent = useSingleFileContent(builder)
+  const files = useMultiFileContent(builder)
+  const singleFileContent = useSingleFileContent(builder)
 
-    // navigate
-    useEffect(() => {
-        if (selectedFile === SINGLE_FILE_NAME) {
-            setOutput(singleFileContent)
-        } else if (selectedFile && files[selectedFile]) {
-            setOutput(files[selectedFile])
-        } else {
-            setOutput('')
-        }
-    }, [setOutput, selectedFile, files, singleFileContent])
+  // navigate
+  useEffect(() => {
+    if (selectedFile === SINGLE_FILE_NAME) {
+      setOutput(singleFileContent)
+    } else if (selectedFile && files[selectedFile]) {
+      setOutput(files[selectedFile])
+    } else {
+      setOutput('')
+    }
+  }, [setOutput, selectedFile, files, singleFileContent])
 
-    useEffect(() => {
-        Prism.highlightAll();
-    })
+  useEffect(() => {
+    Prism.highlightAll();
+  })
 
-    const createZip = useCallback(async () => {
-        const zip = new JSZip();
-        for (let fileName in files) {
-            zip.file(fileName, files[fileName]);
-        }
-        const zipContent = await zip.generateAsync({type: "blob"})
-        saveAs(zipContent, "content-types.zip")
-    }, [files])
+  const createZip = useCallback(async () => {
+    const zip = new JSZip();
+    for (let fileName in files) {
+      zip.file(fileName, files[fileName]);
+    }
+    const zipContent = await zip.generateAsync({type: "blob"})
+    saveAs(zipContent, "content-types.zip")
+  }, [files])
 
-    const createFile = useCallback(async () => {
-        const content = new Blob([singleFileContent], {type: "text/plain;charset=utf-8"})
-        saveAs(content, SINGLE_FILE_NAME)
-    }, [singleFileContent])
+  const createFile = useCallback(async () => {
+    const content = new Blob([singleFileContent], {type: "text/plain;charset=utf-8"})
+    saveAs(content, SINGLE_FILE_NAME)
+  }, [singleFileContent])
 
-    return (
-        <>
-            <GlobalStyles/>
-            <Workbench>
-                <Workbench.Header
-                    title={'Content Types (TS) Generator'}
-                    description={'Generate Typescript type definitions based on content types'}
-                />
-                <Workbench.Content type={"full"}>
-                    {isLoadingUsers ? <Spinner>Loading meta data</Spinner> :
-                        <>
-                            <CopyButton className={styles.copyButton} value={output}/>
-                            <pre><code className={'lang-typescript'}>{output}</code></pre>
-                        </>
-                    }
-                </Workbench.Content>
-                <Workbench.Sidebar position="right">
+  return (
+    <>
+      <GlobalStyles/>
+      <Workbench>
+        <Workbench.Header
+          title={'Content Types (TS) Generator'}
+          description={'Generate Typescript type definitions based on content types'}
+        />
+        <Workbench.Content type={"full"}>
+          {isLoadingUsers ? <Spinner>Loading meta data</Spinner> :
+            <>
+              <CopyButton className={styles.copyButton} value={output}/>
+              <pre><code className={'lang-typescript'}>{output}</code></pre>
+            </>
+          }
+        </Workbench.Content>
+        <Workbench.Sidebar position="right">
 
-                    <SidebarSection title={'Config'} isNew={true}>
+          <SidebarSection title={'Config'} isNew={true}>
 
-                        <Checkbox
-                            title="V10"
-                            helpText="contentful@v10"
-                            name="contentful@10"
-                            onChange={() => setIsV10((value) => !value)}
-                            isChecked={isV10}
-                            value={'checked'}
-                            id="v10"
-                        >v10</Checkbox>
+            <Checkbox
+              title="V10"
+              helpText="contentful@v10"
+              name="contentful@10"
+              onChange={() => setIsV10((value) => !value)}
+              isChecked={isV10}
+              value={'checked'}
+              id="v10"
+            >v10</Checkbox>
 
-                        <FlagsConfiguration onSelect={setFlags} selected={flags} isV10={isV10}/>
-                    </SidebarSection>
+            <FlagsConfiguration onSelect={setFlags} selected={flags} isV10={isV10}/>
+          </SidebarSection>
 
-                    <SidebarSection title={'single File'}>
-                        <FilesNavigation selected={selectedFile} files={[SINGLE_FILE_NAME]}
-                                         onSelect={setSelectedFile}/>
-                    </SidebarSection>
-                    <SidebarSection title={'Files'}>
-                        <FilesNavigation selected={selectedFile} files={Object.keys(files)}
-                                         onSelect={setSelectedFile}/>
-                    </SidebarSection>
+          <SidebarSection title={'single File'}>
+            <FilesNavigation selected={selectedFile} files={[SINGLE_FILE_NAME]}
+                             onSelect={setSelectedFile}/>
+          </SidebarSection>
+          <SidebarSection title={'Files'}>
+            <FilesNavigation selected={selectedFile} files={Object.keys(files)}
+                             onSelect={setSelectedFile}/>
+          </SidebarSection>
 
-                    <SidebarSection title={'Downloads'}>
-                        <IconButton
-                            aria-label={"Download"}
-                            variant={'primary'}
-                            icon={<DownloadIcon/>}
-                            className={styles.downloadButton}
-                            onClick={createZip}
-                            isFullWidth
-                        >
-                            Download zip
-                        </IconButton>
-                        <Typography>
-                            <Paragraph>
-                                Download a zip file including separate files for each content type.
-                            </Paragraph>
-                        </Typography>
+          <SidebarSection title={'Downloads'}>
+            <IconButton
+              aria-label={"Download"}
+              variant={'primary'}
+              icon={<DownloadIcon/>}
+              className={styles.downloadButton}
+              onClick={createZip}
+              isFullWidth
+            >
+              Download zip
+            </IconButton>
+            <Typography>
+              <Paragraph>
+                Download a zip file including separate files for each content type.
+              </Paragraph>
+            </Typography>
 
-                        <IconButton
-                            aria-label={"Download"}
-                            variant={'primary'}
-                            icon={<DownloadIcon/>}
-                            className={styles.downloadButton}
-                            onClick={createFile}
-                            isFullWidth
-                        >
-                            Download file
-                        </IconButton>
-                        <Typography>
-                            <Paragraph>
-                                Download a single file with all types combined.
-                            </Paragraph>
-                        </Typography>
-                    </SidebarSection>
+            <IconButton
+              aria-label={"Download"}
+              variant={'primary'}
+              icon={<DownloadIcon/>}
+              className={styles.downloadButton}
+              onClick={createFile}
+              isFullWidth
+            >
+              Download file
+            </IconButton>
+            <Typography>
+              <Paragraph>
+                Download a single file with all types combined.
+              </Paragraph>
+            </Typography>
+          </SidebarSection>
 
-                    <SidebarSection title={'Feedback'}>
-                        <Typography>
-                            <Paragraph>
-                                You have ideas on how to improve this tool or things are not working as expected? Please file a <a
-                                target={'_top'}
-                                href={'https://github.com/marcolink/cf-content-types-generator-app/issues'}>Github issue</a>!
-                            </Paragraph>
-                        </Typography>
-                    </SidebarSection>
+          <SidebarSection title={'Feedback'}>
+            <Typography>
+              <Paragraph>
+                You have ideas on how to improve this tool or things are not working as expected? Please file a <a
+                target={'_top'}
+                href={'https://github.com/marcolink/cf-content-types-generator-app/issues'}>Github issue</a>!
+              </Paragraph>
+            </Typography>
+          </SidebarSection>
 
-                    <SidebarSection title={'Project Info'}>
-                        <Typography>
+          <SidebarSection title={'Project Info'}>
+            <Typography>
 
-                            <Paragraph>
-                                <strong>Author:</strong> <a
-                                rel="noopener noreferrer"
-                                target={'_top'}
-                                href={'https://github.com/marcolink'}>Marco Link</a>
-                            </Paragraph>
+              <Paragraph>
+                <strong>Author:</strong> <a
+                rel="noopener noreferrer"
+                target={'_top'}
+                href={'https://github.com/marcolink'}>Marco Link</a>
+              </Paragraph>
 
-                            <Paragraph>
-                                Use <a
-                                rel="noopener noreferrer"
-                                target={'_blank'}
-                                href={'https://github.com/contentful-labs/cf-content-types-generator'}>CLI</a> to integrate directly with your workflow CI/CD.
-                            </Paragraph>
-                        </Typography>
-                    </SidebarSection>
+              <Paragraph>
+                Use <a
+                rel="noopener noreferrer"
+                target={'_blank'}
+                href={'https://github.com/contentful-labs/cf-content-types-generator'}>CLI</a> to integrate directly
+                with your workflow CI/CD.
+              </Paragraph>
+            </Typography>
+          </SidebarSection>
 
-                </Workbench.Sidebar>
-            </Workbench>
-        </>
-    );
+        </Workbench.Sidebar>
+      </Workbench>
+    </>
+  );
 };
 
 export default Page;
